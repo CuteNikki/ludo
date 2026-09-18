2b5d9531449fcb79ed411703728bb71f45667563
Nikki <bravonikki13@gmail.com>
2026-09-18 18:05:35 +0200

    fix: tooltip too close to the edge of the screen

--- apps/web/src/components/ui/tooltip.tsx
+++ apps/web/src/components/ui/tooltip.tsx
@@ -18,7 +18,7 @@ const TooltipContent = forwardRef<ComponentRef<typeof Content>, React.ComponentP
         ref={ref}
         sideOffset={sideOffset}
         className={cn(
-          'z-50 max-w-64 border-2 border-border bg-background-alternative px-3 py-2 text-left text-xs font-medium leading-5 text-foreground shadow-card animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-[--radix-tooltip-content-transform-origin]',
+          'z-50 max-w-64 m-2 border-2 border-border bg-background-alternative px-3 py-2 text-left text-xs font-medium leading-5 text-foreground shadow-card animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-[--radix-tooltip-content-transform-origin]',
           className,
         )}
         {...props}
