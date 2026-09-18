d1c73d724c5e012fb70ec1f65c24f983586fab33
Nikki <bravonikki13@gmail.com>
2026-09-18 18:04:07 +0200

    fix: switch thumb offset

--- apps/web/src/components/ui/switch.tsx
+++ apps/web/src/components/ui/switch.tsx
@@ -8,7 +8,7 @@ import { cn } from '@/lib/utils';
 const Switch = forwardRef<ComponentRef<typeof Root>, React.ComponentPropsWithoutRef<typeof Root>>(({ className, ...props }, ref) => (
   <Root
     className={cn(
-      'peer relative inline-flex h-7 w-12 shrink-0 cursor-pointer items-center rounded-full border-2 border-border transition-colors focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-foreground disabled:cursor-not-allowed disabled:opacity-60 data-[state=checked]:bg-green-500 data-[state=unchecked]:bg-background',
+      'peer relative inline-flex h-7 w-12 shrink-0 cursor-pointer items-center rounded-2xl border-2 border-border transition-colors focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-foreground disabled:cursor-not-allowed disabled:opacity-60 data-[state=checked]:bg-green-500 data-[state=unchecked]:bg-red-300',
       className,
     )}
     {...props}
@@ -16,7 +16,7 @@ const Switch = forwardRef<ComponentRef<typeof Root>, React.ComponentPropsWithout
   >
     <Thumb
       className={cn(
-        'pointer-events-none block h-5 w-5 rounded-full border-2 border-border bg-background-alternative transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0',
+        'pointer-events-none block h-5 w-5 rounded-full border-2 border-border bg-background-alternative transition-transform data-[state=checked]:translate-x-5.5 data-[state=unchecked]:translate-x-0.5',
       )}
     />
   </Root>
